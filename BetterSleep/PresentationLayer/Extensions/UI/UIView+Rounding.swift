b13d6e896540a8c59848aec5e5f3628a510408d2
Andrew Kochulab <andrew.kochulab@gmail.com>
2019-01-13 13:47:42 +0200

    Adde base extensions for UI elements.

---
+++ BetterSleep/PresentationLayer/Extensions/UI/UIView+Rounding.swift
@@ -0,0 +1,38 @@
+//
+//  UIView+Rounding.swift
+//  BetterSleep
+//
+//  Created by Andrew Kochulab on 1/6/19.
+//  Copyright © 2019 Andrew Kochulab. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    
+    // MARK: - Rounding
+    
+    func round(
+        corners: UIRectCorner = .allCorners,
+        radius: Double
+    ) {
+        let path = UIBezierPath(
+            roundedRect: bounds,
+            byRoundingCorners: corners,
+            cornerRadii: CGSize(width: radius, height: radius)
+        )
+        
+        let maskLayer = CAShapeLayer()
+        maskLayer.frame = bounds
+        maskLayer.path = path.cgPath
+        
+        layer.mask = maskLayer
+    }
+    
+    func round(
+        radius: CGFloat
+    ) {
+        layer.masksToBounds = true
+        layer.cornerRadius = radius
+    }
+}
