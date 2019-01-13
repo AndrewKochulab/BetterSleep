98cbd87a2f4b98ce87d70e492fceedc1edef78f0
Andrew Kochulab <andrew.kochulab@gmail.com>
2019-01-13 23:49:22 +0200

    Started implementing `Settings` tab bar module.

---
+++ BetterSleep/PresentationLayer/Modules/Settings/Controller/SettingsViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SettingsViewController.swift
+//  BetterSleep
+//
+//  Created by Andrew Kochulab on 1/13/19.
+//  Copyright © 2019 Andrew Kochulab. All rights reserved.
+//
+
+import UIKit
+
+final class SettingsViewController: ViewController<
+    SettingsControllerView,
+    SettingsControllerViewModel
+> {
+    
+}
---
+++ BetterSleep/PresentationLayer/Modules/Settings/View/SettingsControllerView.swift
@@ -0,0 +1,13 @@
+//
+//  SettingsControllerView.swift
+//  BetterSleep
+//
+//  Created by Andrew Kochulab on 1/13/19.
+//  Copyright © 2019 Andrew Kochulab. All rights reserved.
+//
+
+import UIKit
+
+final class SettingsControllerView: ControllerView {
+    
+}
